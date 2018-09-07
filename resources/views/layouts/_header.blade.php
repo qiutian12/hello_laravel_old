cb2147cafdd6500d4f206687f10692b805de2c83
qiutian <qiutian@qq.com>
2018-09-07 02:16:11 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-default">
+    <div class="" id="navbarSupportedContent">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="{{ route('home') }}">首页</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('about') }}">关于</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('help') }}">帮助</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('signup') }}">注册</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">登录</a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,14 @@
 <html>
 <head>
     <title>@yield('title', 'Sample') - Laravel</title>
+    <link rel="stylesheet" href="/bootstrap4/css/bootstrap.css">
 </head>
 <body>
-    @yield('content')
+    <div class="container">
+        @include('layouts._header')
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
 </html>
